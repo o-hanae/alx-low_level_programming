7a075347b22822cd45d37b0f01771d6544b631bb
o-hanae <h.ouahab25@gmail.com>
2023-02-16 13:58:18 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - this prints hello
+  * Return: This returns 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
