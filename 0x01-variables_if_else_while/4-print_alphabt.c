83c678af17114f5de5d0b2c56f5006e52d1d2579
o-hanae <h.ouahab25@gmail.com>
2023-02-17 09:44:33 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char alp = 'a';
+
+while (alp <= 'z')
+{
+if (alp != 'e' && alp != 'q')
+{
+putchar(alp);
+}
+}
+alp++;
+putchar('\n');
+return (0);
+}
