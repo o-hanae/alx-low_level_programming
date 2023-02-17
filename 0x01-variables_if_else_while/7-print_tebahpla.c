471b0542a2165a9b8c6547887a5c25f86b12fec0
o-hanae <h.ouahab25@gmail.com>
2023-02-17 10:35:45 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'z'; az >= 'a'; az--)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
