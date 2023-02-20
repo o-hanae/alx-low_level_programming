0086b366b92f1ebb41a5df315b658b7fe96c880c
o-hanae <h.ouahab25@gmail.com>
2023-02-20 12:57:12 -0800

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 8; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			if (b > a)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				if (a < 8 || b < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
