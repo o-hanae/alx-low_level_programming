53894ecccb09f40b51c177cd69fba555f3e9c644
o-hanae <h.ouahab25@gmail.com>
2023-02-21 11:49:12 -0800

    add task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add-check if character is do the some of two number
+ * @a:checker
+ * @b:checker
+ * Return:the some
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
