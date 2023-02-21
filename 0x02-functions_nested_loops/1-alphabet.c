9f0c444e7c2c7b7a7d83172547f66ddbdf79283f
o-hanae <h.ouahab25@gmail.com>
2023-02-21 08:10:48 -0800

    tsk2 alphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = a; alp <= z; alp++)
+	{
+		_putchar('alp');
+	}
+}
