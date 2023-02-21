47fa9c094eb63a3a7cd4968737692940397375a1
o-hanae <h.ouahab25@gmail.com>
2023-02-21 08:55:55 -0800

    task 2 print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabets
+ * Return: 1
+ */
+void print_alphabet_x10(void)
+{
+	char alp;
+	int x;
+
+	for (x = 0; x <= 9, x++)
+	{
+		for (alp = 'a'; alp = 'z'; apl++)
+		{
+			_putchar(alp);
+		}
+		putchar('\n');
+	}
+}
