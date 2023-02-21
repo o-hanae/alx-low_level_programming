28824e55f6baa42786b0ebcb63274a6caf7ce3ff
o-hanae <h.ouahab25@gmail.com>
2023-02-21 12:20:55 -0800

    the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+include "main.h"
+
+/**
+ * print_last_digit-check if character is write the last digit
+ * @n:checker
+ * Return:the last digit.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
