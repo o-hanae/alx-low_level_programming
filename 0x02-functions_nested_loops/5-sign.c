75c8d4df52ba81e672a3c994054aafc17f6be71c
o-hanae <h.ouahab25@gmail.com>
2023-02-21 10:56:04 -0800

    print the sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign- check if character is positive , negatif or 0.
+ * @n:character checker.
+ * Return:0-if zero,1-if positif ,-1-if negatif.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
