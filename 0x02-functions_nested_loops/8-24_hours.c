e726cbfb51407cbf5192fb0d4b95dda2e0f48fef
o-hanae <h.ouahab25@gmail.com>
2023-02-22 07:26:44 -0800

    time 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer-prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar('0' + x / 10);
+			_putchar('0' + x % 10);
+			_putchar(':');
+			_putchar('0' + y / 10);
+			_putchar('0' + y % 10);
+			_putchar('\n');
+		}
+	}
+}
