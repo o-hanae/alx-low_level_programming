0516b43a4fcc3b32e6956f6017a1b594f82eb704
o-hanae <h.ouahab25@gmail.com>
2023-03-08 15:49:47 -0800

    task 5 unction that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,5 +9,20 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (squareroot(n, (n + 1) / 2));
+		return (sqrtroot(n, (n + 1) / 2));
+}
+/**
+ * sqrtroot-checks if perfect square
+ * @n:input
+ * @i:counter
+ * Return:if square root
+ */
+int sqrtroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (sqrtroot(n, i - 1));
 }
