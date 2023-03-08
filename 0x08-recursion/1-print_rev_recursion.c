5a351b6fc23b9846c6232b9802fa6a0c123ee9a2
o-hanae <h.ouahab25@gmail.com>
2023-03-08 06:21:01 -0800

    task1 function that prints a string in reverse.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion-function that prints a string in reverse.
+ * @s:character
+ * Return:nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s - 1);
+	}
+}
