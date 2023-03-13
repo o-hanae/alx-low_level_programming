8779e778875d5dc4634bfba849413553d79e172b
o-hanae <h.ouahab25@gmail.com>
2023-03-13 11:21:16 -0700

    task0 static libaries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string,followed  by a new line,to stdout
+ * @s: this is my input string
+ * Return: void
+ */
+
+void _puts(char *s)
+{
+	_puts(s);
+}
