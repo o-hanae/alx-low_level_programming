697e51ffd3a0c77b68494be090ceb6f632fc02e6
o-hanae <h.ouahab25@gmail.com>
2023-03-14 09:48:04 -0700

    task1 aegument argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main:program that prints the number of arguments passed into it.
+ * argc:number of commande
+ * argv:number of line commande
+ * Return:something
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n" ,argc);
+	return (0);
+}
