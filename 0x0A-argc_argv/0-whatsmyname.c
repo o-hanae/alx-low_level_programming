257b035e9d216134fc6fa1d1c793cca034b71232
o-hanae <h.ouahab25@gmail.com>
2023-03-14 09:01:58 -0700

    tsk0 argmeun v

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main-program that prints its name
+ * @argc:number of comnd line
+ * @argv:array of commande line
+ * Return:something
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+readme of argument argc and argv
