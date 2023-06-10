43e151cd54f72d00649a06acb3235f8d6135bbdb
o-hanae <h.ouahab25@gmail.com>
2023-06-09 17:14:56 -0700

    tsk3 mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - Prints all the arguments it receives
- * @argc: Argument count
- * @argv: Argument vector
- * Return: Always 0
+ * main - Multiplies first integer arguments
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in. 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < argc; i++)
-	printf("%s\n", argv[i]);
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+		j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
 
 	return (0);
 }
