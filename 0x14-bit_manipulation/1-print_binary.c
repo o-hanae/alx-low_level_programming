0f9070264a5505ec16471ea583e306d062970173
o-hanae <h.ouahab25@gmail.com>
2023-07-06 11:08:48 -0700

    tsk 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - function to print binary repr of a number
+ * Description:
+ * @n: parameter
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int b, count = 0;
+	unsigned long int present;
+
+	for (b = 63; b >= 0; b--)
+	{
+		present = n >> b;
+
+		if (present & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
